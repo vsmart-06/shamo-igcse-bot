b8542ca6ee4ceabe394ec627e57b0218820dc6d0
vsmart-06 <srivishnuvusirikala@gmail.com>
2022-09-25 15:40:23 +0530

    Added activity status

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ helper_roles = {994617607862878278: 994618740534689884, 994617634719023234: 9946
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity = discord.Game(name = "DM to contact moderators"))
     print("Your wish is my command!")
     my_user = await bot.fetch_user(706855396828250153)
     await my_user.send("Ready!")
